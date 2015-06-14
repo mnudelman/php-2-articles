ba03c0a808e9231a24b89f456b5aa32f207e7d83
mnudelman <mnudelman@yandex.ru>
2015-06-14 22:00:19 +0500

    14-06-2015T22:00

--- appl/view/vw_articleList.php
+++ appl/view/vw_articleList.php
@@ -6,19 +6,21 @@
  */
 ?>
 <ul>
-<?php
-foreach($articles as  $article ){
-    $aid = $article['articleid'];
-    $title = $article['title'];
-    $file = $article['file'];
-    $topics = $article['topics'];
+    <?php
+    if (is_array($articles)) {
+        foreach ($articles as $article) {
+            $aid = $article['articleid'];
+            $title = $article['title'];
+            $file = $article['file'];
+            $topics = $article['topics'];
+            ?>
+            <li>
+                <a class="rightMenu" href="<?= $htmlDirTop; ?>/index.php?cnt=Cnt_navigator&articleid=<?= $aid; ?>">
+                    <strong> <?= $title; ?> </strong>
+                </a>
+            </li>
+        <?php
+        }
+    }
     ?>
-    <li>
-        <a class="rightMenu" href="<?=$htmlDirTop;?>/index.php?cnt=Cnt_navigator&articleid=<?=$aid;?>" >
-       <strong> <?=$title;?> </strong>
-        </a>
-    </li>
-<?php
-}
-?>
 </ul>
